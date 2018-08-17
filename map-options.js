daf6e394b69588708b1e44120244a56ba9adbcfd
Raghav1606 <raghavkapoor16@hotmail.com>
2018-08-17 21:59:42 +0530

    Maps Implemented

--- map-options.js
+++ map-options.js
@@ -10,39 +10,6 @@
             style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
             position: google.maps.ControlPosition.TOP_RIGHT
         }
-        // disableDefaultUI: false,
-        // scrollwheel: true,
-        // draggable: true,
-        // mapTypeId: google.maps.MapTypeId.ROADMAP,
-        // mapTypeId: google.maps.MapTypeId.HYBRID,
-        // mapTypeId: google.maps.MapTypeId.SATELLITE,
-        // maxzoom: 11,
-        // minzoom: 9,
-        // zoomControlOptions: {
-        //     position: google.maps.ControlPosition.BOTTOM_RIGHT,
-        //     style: google.maps.ZoomControlStyle.SMALL, // or DEFAULT
-        // },
-        // panControlOptions: {
-        //     position: google.maps.ControlPosition.BOTTOM_RIGHT,
-        // }
-        // cluster: {
-        //     options: {
-        //         styles: [{
-        //             url: 'clusterers/m1.png',
-        //             height: 56,
-        //             width: 55,
-        //             textColor: '#f00',
-        //             textSize: 14
-        //         },
-        //         {
-        //             url: 'clusterers/m2.png',
-        //             height: 56,
-        //             width: 55,
-        //             textColor: '#f00',
-        //             textSize: 14
-        //         }]
-        //     }
-        // }
     };
 
 }(window, google, window.SafeRoutes || (window.SafeRoutes = {})));
